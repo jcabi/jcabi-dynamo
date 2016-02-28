b12edf525ef2bdc01be6c93885a9e58f492eac7a
Yegor Bugayenko <yegor@tpc2.com>
2016-02-28 08:58:52 +0200

    extra test

---
+++ src/test/java/com/jcabi/dynamo/AwsItemTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2012-2015, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.dynamo;
+
+import com.jcabi.immutable.Array;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Test case for {@link AwsItem}.
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
+ * @since 0.21
+ * @checkstyle ClassDataAbstractionCoupling (500 lines)
+ */
+public final class AwsItemTest {
+
+    /**
+     * AwsTable can compare to itself.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void comparesToItself() throws Exception {
+        final Credentials creds = new Credentials.Simple("key", "secret");
+        final Region region = new Region.Simple(creds);
+        final AwsTable table = new AwsTable(creds, region, "table-name");
+        final AwsFrame frame = new AwsFrame(creds, table, table.name());
+        MatcherAssert.assertThat(
+            new AwsItem(
+                creds, frame, table.name(),
+                new Attributes(), new Array<String>()
+            ),
+            Matchers.equalTo(
+                new AwsItem(
+                    creds, frame, table.name(),
+                    new Attributes(), new Array<String>()
+                )
+            )
+        );
+    }
+
+}
