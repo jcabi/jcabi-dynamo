80e3c41fa6130778d21398cb387dfd8dbf86b6ae
Yegor Bugayenko <yegor256@gmail.com>
2026-02-05 12:48:35 +0300

    #441: Add tests for preloaded attribute handling in AwsItem.

--- src/test/java/com/jcabi/dynamo/AwsItemTest.java
+++ src/test/java/com/jcabi/dynamo/AwsItemTest.java
@@ -5,14 +5,30 @@
 package com.jcabi.dynamo;
 
 import com.jcabi.immutable.Array;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.NoSuchElementException;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeAction;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValueUpdate;
+import software.amazon.awssdk.services.dynamodb.model.ConsumedCapacity;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemResponse;
 
 /**
  * Test case for {@link AwsItem}.
  * @since 0.21
  */
+@SuppressWarnings("PMD.TooManyMethods")
 final class AwsItemTest {
 
     @Test
@@ -35,6 +51,321 @@ final class AwsItemTest {
                 )
             )
         );
+    }
+
+    @Test
+    void checksPreloadedAttribute() throws IOException {
+        MatcherAssert.assertThat(
+            "did not find pre-loaded attribute",
+            new AwsItem(
+                Mockito.mock(Credentials.class),
+                Mockito.mock(AwsFrame.class),
+                "t\u00e9st-tbl",
+                new Attributes().with(
+                    "k\u00e9y",
+                    AttributeValue.builder().s("v\u00e1l").build()
+                ),
+                new Array<>("k\u00e9y")
+            ).has("k\u00e9y"),
+            Matchers.is(true)
+        );
+    }
+
+    @Test
+    void returnsPreloadedAttribute() throws IOException {
+        MatcherAssert.assertThat(
+            "did not return pre-loaded attribute value",
+            new AwsItem(
+                Mockito.mock(Credentials.class),
+                Mockito.mock(AwsFrame.class),
+                "t\u00e9st-tbl",
+                new Attributes().with(
+                    "n\u00e4me",
+                    AttributeValue.builder().s("d\u00e4ta").build()
+                ),
+                new Array<>("n\u00e4me")
+            ).get("n\u00e4me"),
+            Matchers.equalTo(
+                AttributeValue.builder().s("d\u00e4ta").build()
+            )
+        );
+    }
+
+    @Test
+    void fetchesAttributeFromDynamo() throws IOException {
+        final Credentials creds = Mockito.mock(Credentials.class);
+        final DynamoDbClient aws = Mockito.mock(DynamoDbClient.class);
+        Mockito.doReturn(aws).when(creds).aws();
+        final String attr = "\u00e4ttr";
+        Mockito.doReturn(
+            GetItemResponse.builder()
+                .item(
+                    Collections.singletonMap(
+                        attr,
+                        AttributeValue.builder().s("r\u00e9sult").build()
+                    )
+                )
+                .consumedCapacity(
+                    ConsumedCapacity.builder().capacityUnits(1.0).build()
+                )
+                .build()
+        ).when(aws).getItem(Mockito.any(GetItemRequest.class));
+        MatcherAssert.assertThat(
+            "did not fetch attribute from DynamoDB",
+            new AwsItem(
+                creds, Mockito.mock(AwsFrame.class), "f\u00e9tch-tbl",
+                new Attributes().with(
+                    "pk",
+                    AttributeValue.builder().s("f\u00e9tch-pk").build()
+                ),
+                new Array<>("pk")
+            ).get(attr),
+            Matchers.equalTo(
+                AttributeValue.builder().s("r\u00e9sult").build()
+            )
+        );
+    }
+
+    @Test
+    void checksExistenceViaDynamo() throws IOException {
+        final Credentials creds = Mockito.mock(Credentials.class);
+        final DynamoDbClient aws = Mockito.mock(DynamoDbClient.class);
+        Mockito.doReturn(aws).when(creds).aws();
+        final String attr = "ch\u00e9ck";
+        Mockito.doReturn(
+            GetItemResponse.builder()
+                .item(
+                    Collections.singletonMap(
+                        attr,
+                        AttributeValue.builder().s("pr\u00e9sent").build()
+                    )
+                )
+                .consumedCapacity(
+                    ConsumedCapacity.builder().capacityUnits(1.0).build()
+                )
+                .build()
+        ).when(aws).getItem(Mockito.any(GetItemRequest.class));
+        MatcherAssert.assertThat(
+            "did not detect attribute existence via DynamoDB",
+            new AwsItem(
+                creds, Mockito.mock(AwsFrame.class), "h\u00e4s-tbl",
+                new Attributes().with(
+                    "pk",
+                    AttributeValue.builder().s("h\u00e4s-pk").build()
+                ),
+                new Array<>("pk")
+            ).has(attr),
+            Matchers.is(true)
+        );
+    }
+
+    @Test
+    void updatesAttribute() throws IOException {
+        final Credentials creds = Mockito.mock(Credentials.class);
+        final DynamoDbClient aws = Mockito.mock(DynamoDbClient.class);
+        Mockito.doReturn(aws).when(creds).aws();
+        final String attr = "\u00fcpd";
+        Mockito.doReturn(
+            UpdateItemResponse.builder()
+                .attributes(
+                    Collections.singletonMap(
+                        attr,
+                        AttributeValue.builder().s("n\u00e9w").build()
+                    )
+                )
+                .consumedCapacity(
+                    ConsumedCapacity.builder().capacityUnits(1.0).build()
+                )
+                .build()
+        ).when(aws).updateItem(Mockito.any(UpdateItemRequest.class));
+        MatcherAssert.assertThat(
+            "did not return updated attributes",
+            new AwsItem(
+                creds, Mockito.mock(AwsFrame.class), "\u00fcpd-tbl",
+                new Attributes().with(
+                    "pk",
+                    AttributeValue.builder().s("\u00fcpd-pk").build()
+                ),
+                new Array<>("pk")
+            ).put(
+                attr,
+                AttributeValueUpdate.builder()
+                    .value(
+                        AttributeValue.builder().s("n\u00e9w").build()
+                    )
+                    .action(AttributeAction.PUT)
+                    .build()
+            ),
+            Matchers.hasEntry(
+                Matchers.equalTo(attr),
+                Matchers.equalTo(
+                    AttributeValue.builder().s("n\u00e9w").build()
+                )
+            )
+        );
+    }
+
+    @Test
+    void throwsOnAbsentAttribute() throws IOException {
+        final DynamoDbClient aws = Mockito.mock(DynamoDbClient.class);
+        Mockito.doReturn(AwsItemTest.empty())
+            .when(aws).getItem(Mockito.any(GetItemRequest.class));
+        final Item item = new AwsItem(
+            AwsItemTest.mocked(aws),
+            Mockito.mock(AwsFrame.class), "abs\u00e9nt-tbl",
+            new Attributes().with(
+                "pk",
+                AttributeValue.builder().s("abs\u00e9nt-pk").build()
+            ),
+            new Array<>("pk")
+        );
+        Assertions.assertThrows(
+            NoSuchElementException.class,
+            () -> item.get("m\u00efssing")
+        );
+    }
+
+    @Test
+    void wrapsExceptionOnGet() {
+        final DynamoDbClient aws = Mockito.mock(DynamoDbClient.class);
+        Mockito.doThrow(SdkClientException.create("b\u00f6om"))
+            .when(aws).getItem(Mockito.any(GetItemRequest.class));
+        final Item item = new AwsItem(
+            AwsItemTest.mocked(aws),
+            Mockito.mock(AwsFrame.class), "g\u00e9t-err",
+            new Attributes().with(
+                "pk",
+                AttributeValue.builder().s("g\u00e9t-pk").build()
+            ),
+            new Array<>("pk")
+        );
+        Assertions.assertThrows(
+            IOException.class,
+            () -> item.get("f\u00e4il")
+        );
+    }
+
+    @Test
+    void wrapsExceptionOnHas() {
+        final DynamoDbClient aws = Mockito.mock(DynamoDbClient.class);
+        Mockito.doThrow(SdkClientException.create("b\u00f6om"))
+            .when(aws).getItem(Mockito.any(GetItemRequest.class));
+        final Item item = new AwsItem(
+            AwsItemTest.mocked(aws),
+            Mockito.mock(AwsFrame.class), "h\u00e4s-err",
+            new Attributes().with(
+                "pk",
+                AttributeValue.builder().s("h\u00e4s-pk").build()
+            ),
+            new Array<>("pk")
+        );
+        Assertions.assertThrows(
+            IOException.class,
+            () -> item.has("f\u00e4il")
+        );
+    }
+
+    @Test
+    void wrapsExceptionOnPut() {
+        final DynamoDbClient aws = Mockito.mock(DynamoDbClient.class);
+        Mockito.doThrow(SdkClientException.create("b\u00f6om"))
+            .when(aws).updateItem(Mockito.any(UpdateItemRequest.class));
+        final Item item = new AwsItem(
+            AwsItemTest.mocked(aws),
+            Mockito.mock(AwsFrame.class), "p\u00fct-err",
+            new Attributes().with(
+                "pk",
+                AttributeValue.builder().s("p\u00fct-pk").build()
+            ),
+            new Array<>("pk")
+        );
+        Assertions.assertThrows(
+            IOException.class,
+            () -> item.put(
+                new AttributeUpdates().with(
+                    "f\u00e4il",
+                    AttributeValueUpdate.builder()
+                        .value(
+                            AttributeValue.builder().s("v\u00e1l").build()
+                        )
+                        .action(AttributeAction.PUT)
+                        .build()
+                )
+            )
+        );
+    }
+
+    @Test
+    void distinguishesDifferentItems() {
+        final Credentials creds = new Credentials.Simple(
+            "k\u00e9y1", "s\u00e9cret1"
+        );
+        final AwsTable first = new AwsTable(
+            creds, new Region.Simple(creds), "t\u00e4ble-one"
+        );
+        final AwsTable second = new AwsTable(
+            creds, new Region.Simple(creds), "t\u00e4ble-two"
+        );
+        MatcherAssert.assertThat(
+            "items with different tables should not be equal",
+            new AwsItem(
+                creds,
+                new AwsFrame(creds, first, first.name()),
+                first.name(),
+                new Attributes(),
+                new Array<>()
+            ),
+            Matchers.not(
+                Matchers.equalTo(
+                    new AwsItem(
+                        creds,
+                        new AwsFrame(creds, second, second.name()),
+                        second.name(),
+                        new Attributes(),
+                        new Array<>()
+                    )
+                )
+            )
+        );
+    }
+
+    @Test
+    void returnsFrame() {
+        MatcherAssert.assertThat(
+            "did not return frame instance",
+            new AwsItem(
+                Mockito.mock(Credentials.class),
+                Mockito.mock(AwsFrame.class),
+                "fr\u00e4me-tbl",
+                new Attributes(),
+                new Array<>()
+            ).frame(),
+            Matchers.instanceOf(Frame.class)
+        );
+    }
+
+    /**
+     * Creates a Credentials mock that returns the given DynamoDbClient.
+     * @param aws DynamoDbClient mock
+     * @return Credentials mock
+     */
+    private static Credentials mocked(final DynamoDbClient aws) {
+        final Credentials creds = Mockito.mock(Credentials.class);
+        Mockito.doReturn(aws).when(creds).aws();
+        return creds;
+    }
+
+    /**
+     * Creates an empty GetItemResponse with consumed capacity.
+     * @return GetItemResponse with no items
+     */
+    private static GetItemResponse empty() {
+        return GetItemResponse.builder()
+            .item(Collections.emptyMap())
+            .consumedCapacity(
+                ConsumedCapacity.builder().capacityUnits(1.0).build()
+            )
+            .build();
     }
 
 }
--- src/test/java/com/jcabi/dynamo/CredentialsTest.java
+++ src/test/java/com/jcabi/dynamo/CredentialsTest.java
@@ -7,6 +7,7 @@ package com.jcabi.dynamo;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 /**
  * Test case for {@link Credentials}.
@@ -16,26 +17,128 @@ final class CredentialsTest {
 
     @Test
     void instantiatesAwsClient() {
+        final DynamoDbClient aws = new Credentials.Simple(
+            "ABABABABABABABABABEF",
+            "ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCEF"
+        ).aws();
         MatcherAssert.assertThat(
-            "not null",
-            new Credentials.Simple(
-                "ABABABABABABABABABEF",
-                "ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCEF"
-            ).aws(),
+            "Simple credentials did not create AWS client",
+            aws,
             Matchers.notNullValue()
         );
+        aws.close();
     }
 
     @Test
     void instantiatesAwsClientWithCustomRegion() {
+        final DynamoDbClient aws = new Credentials.Simple(
+            "ABABABABABABABABABAB",
+            "ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE",
+            "eu-west-1"
+        ).aws();
         MatcherAssert.assertThat(
-            "not null",
-            new Credentials.Simple(
-                "ABABABABABABABABABAB",
-                "ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE",
-                "eu-west-1"
-            ).aws(),
+            "Simple credentials with region did not create AWS client",
+            aws,
             Matchers.notNullValue()
+        );
+        aws.close();
+    }
+
+    @Test
+    void formatsSimpleAsString() {
+        MatcherAssert.assertThat(
+            "Simple credentials did not format as region/key",
+            new Credentials.Simple(
+                "t\u00e9st-k\u00e9y-000000000",
+                "s\u00e9cr\u00e9t-0000000000000000000000000000",
+                "us-\u00e9ast-1"
+            ).toString(),
+            Matchers.equalTo("us-\u00e9ast-1/t\u00e9st-k\u00e9y-000000000")
+        );
+    }
+
+    @Test
+    void instantiatesAssumedClient() {
+        final DynamoDbClient aws = new Credentials.Assumed().aws();
+        MatcherAssert.assertThat(
+            "Assumed credentials did not create AWS client",
+            aws,
+            Matchers.notNullValue()
+        );
+        aws.close();
+    }
+
+    @Test
+    void instantiatesAssumedClientWithRegion() {
+        final DynamoDbClient aws =
+            new Credentials.Assumed("eu-west-1").aws();
+        MatcherAssert.assertThat(
+            "Assumed credentials with region did not create AWS client",
+            aws,
+            Matchers.notNullValue()
+        );
+        aws.close();
+    }
+
+    @Test
+    void formatsAssumedAsString() {
+        MatcherAssert.assertThat(
+            "Assumed credentials did not format as region",
+            new Credentials.Assumed("us-w\u00e9st-2").toString(),
+            Matchers.equalTo("us-w\u00e9st-2")
+        );
+    }
+
+    @Test
+    void instantiatesDirectClient() {
+        final DynamoDbClient aws = new Credentials.Direct(
+            new Credentials.Simple(
+                "ABABABABABABABABAB00",
+                "ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABC00"
+            ),
+            "http://localhost:8080"
+        ).aws();
+        MatcherAssert.assertThat(
+            "Direct credentials did not create AWS client",
+            aws,
+            Matchers.notNullValue()
+        );
+        aws.close();
+    }
+
+    @Test
+    void instantiatesDirectClientWithPort() {
+        final DynamoDbClient aws = new Credentials.Direct(
+            new Credentials.Simple(
+                "ABABABABABABABABAB01",
+                "ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABC01"
+            ),
+            12_345
+        ).aws();
+        MatcherAssert.assertThat(
+            "Direct credentials with port did not create AWS client",
+            aws,
+            Matchers.notNullValue()
+        );
+        aws.close();
+    }
+
+    @Test
+    void formatsDirectAsString() {
+        MatcherAssert.assertThat(
+            "Direct credentials did not format with origin and endpoint",
+            new Credentials.Direct(
+                new Credentials.Simple(
+                    "ABABABABABABABABAB02",
+                    "ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABC02",
+                    "us-\u00e9ast-1"
+                ),
+                "http://loc\u00e4l:9999"
+            ).toString(),
+            Matchers.allOf(
+                Matchers.containsString("us-\u00e9ast-1"),
+                Matchers.containsString("http://loc\u00e4l:9999")
+            )
         );
     }
 
